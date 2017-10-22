3b79b987bf47068b5e1d3c8e0d9013d5cc999a62
GValas <valasg@gmail.com>
2017-10-22 17:47:12 +0200

    added new KNN model to estimate flats prices

--- scripts/mongo_request.js
+++ scripts/mongo_request.js
@@ -3,3 +3,10 @@ db.annonces.aggregate([
     {"$group" : {_id: "$annonce.idannonce", count:{$sum:1}}},
     {"$match":  { count: { $gt: 1 } } }
 ])
+
+
+// select idannonce, count(idannonce) as count from annonces group by idannonce having count > 1
+db.annonces.aggregate([
+    {"$group" : {_id: "$annonce.cp", count:{$sum:1}}} ,	
+    {"$orderby":{ "_id": -1 }
+]).sort( { count: -1 } )
